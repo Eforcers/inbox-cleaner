662a0739bf13559431aaaccfd739587a168e9487
jose.chio <jose.chio@eforcers.com>
2014-06-06 16:13:36 -0500

    Finished pipeline structure

--- src/inbox/pipelines.py
+++ src/inbox/pipelines.py
@@ -5,19 +5,43 @@ import pipeline
 from pipeline.common import List, Return
 import logging
 from helpers import IMAPHelper
+from models import MoveProcess, MoveUserProcess
+from tasks import get_messages, move_message
 
 class MoveProcessPipeline(pipeline.Pipeline):
     def run(self, move_process_id):
         logging.info("start process for move_process %s", move_process_id)
-        pass
+        process = MoveProcess.get_by_id(move_process_id)
+        emails = process.emails
 
-class MoveMessageBatchProcess(pipeline.Pipeline):
-    def run(self, email):
-        imap = IMAPHelper()
-        imap.oauth1_2lo_login(user_email=email)
-        pass
+        user_processes = []
+        for email in emails:
+            user_process = MoveUserProcess(
+                user_email = email,
+                move_process_key = process.key
+            )
+            user_process_key = user_process.put()
+            user_process_id = user_process_key.id()
 
-class MoveMessageProcess(pipeline.Pipeline):
-    def run(self, msg_id):
-        pass
+            user_processes.append((yield MoveUserProcessPipeline(
+                user_process_id=user_process_id)))
+
+        yield List(*user_process)
+
+class MoveUserProcessPipeline(pipeline.Pipeline):
+    def run(self, user_process_id=None):
+        user_process = MoveUserProcess.get_by_id(user_process_id)
+        messages = get_messages(user_process)
+        message_processes = []
+        for batch in messages:
+            message_processes.append((yield MoveMessageProcessPipeline(
+                batch=batch, user_process_id=user_process_id)))
+        yield List(*message_processes)
+
+class MoveMessageProcessPipeline(pipeline.Pipeline):
+    def run(self, batch=None, user_process_id=None):
+        user_process = MoveUserProcess.get_by_id(user_process_id)
+        move_process = user_process.move_process_key.get()
+        for msg_id in batch:
+            move_message(user_process=user_process, msg_id=msg_id, label=move_process.tag)
 
--- src/inbox/tasks.py
+++ src/inbox/tasks.py
@@ -1,4 +1,46 @@
 # -*- coding: utf-8 -*-
+from models import MoveUserProcess
+from helpers import IMAPHelper
+import constants
+from models import MoveMessageProcess
+from livecount import counter
+
+def get_messages(user_process=None):
+    imap = IMAPHelper()
+    imap.oauth1_2lo_login(user_email=user_process.email)
+    msg_ids = imap.list_messages(only_from_trash=True)
+
+    n = constants.MESSAGE_BATCH_SIZE
+    return [msg_ids[i:i+n] for i in range(0, len(msg_ids), n)]
+
+def move_message(user_process=None, msg_id=None, label=None):
+    message_process = MoveMessageProcess(email=user_process.email,
+                       message_id=msg_id,
+                       user_process_key=user_process.key,
+                       error_description='')
+
+    try:
+        imap = IMAPHelper()
+        imap.oauth1_2lo_login(user_email=user_process.email)
+        result, data = imap.copy_message(msg_id=msg_id, new_label=label)
+
+        if result == 'OK':
+            counter.load_and_increment_counter(
+                '%s_%s_ok_counter' % (user_process.email, user_process.key.id()))
+            message_process.status = constants.FINISHED
+        else:
+            counter.load_and_increment_counter(
+                '%s_%s_error_counter' % (user_process.email, user_process.key.id()))
+            message_process.status = constants.FAILED
+            message_process.error_description = result
+    except Exception as e:
+        counter.load_and_increment_counter(
+                '%s_%s_error_counter' % (user_process.email, user_process.key.id()))
+        message_process.status = constants.FAILED
+        message_process.error_description = e.message
+    message_process.put()
+
+
 
 
 
